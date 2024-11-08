4d778b14060ba9fe5f7c4442e1df32a3241f25ed
Ivan Barrios <ivanbar8112003@gmail.com>
2024-11-08 09:59:14 -0300

    login/register

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
-import Link from 'next/link'
-import './globals.css'
+import Link from "next/link";
+import "./globals.css";
 
 export const metadata = {
-  title: 'FoodReview App',
-  description: 'Upload and view food reviews on a map',
-}
+  title: "FoodReview App",
+  description: "Upload and view food reviews on a map",
+};
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-100 min-h-screen flex flex-col text-black">
@@ -25,9 +25,7 @@ export default function RootLayout({children}) {
             </div>
           </nav>
         </header>
-        <main className="container mx-auto px-4 py-8 flex-grow">
-          {children}
-        </main>
+        <main className="container flex-grow">{children}</main>
         <footer className="bg-white shadow-sm mt-8">
           <div className="container mx-auto px-4 py-4 text-center text-gray-600">
             © 2023 FoodReview App
@@ -35,5 +33,5 @@ export default function RootLayout({children}) {
         </footer>
       </body>
     </html>
-  )
+  );
 }
---
+++ app/register/page.js
@@ -0,0 +1,12 @@
+import Register from "@/components/register/register";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="">
+      <Register />
+    </div>
+  );
+};
+
+export default page;
