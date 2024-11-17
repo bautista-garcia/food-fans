e67484f8e06915e220c0f6e73fd50dd0df0648c9
Ivan Barrios <ivanbar8112003@gmail.com>
2024-11-17 12:30:19 -0300

    fixed /restaurante error

--- app/restaurante/page.js
+++ app/restaurante/page.js
@@ -1,8 +1,9 @@
+"use client";
 import { Star, MapPin, ExternalLink } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-import Badge from "@/components/ui/badge";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 
